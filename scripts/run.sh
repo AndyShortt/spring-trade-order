bb640b5d6757535bb16d82b7b8e287bb8cab1f5d
Andy Shortt <AndrewMShortt@Gmail.com>
2019-01-04 17:13:53 -0500

    typo in service name

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 sudo exec &> run.out
 
 sudo link -s /usr/bin/spring-trade-service-0.1.0.jar /etc/init.d/spring-trade-service
-/etc/init.d/myapp start
+/etc/init.d/spring-trade-service start
 
 #cd /usr/bin
 #nohup java -jar spring-trade-service-0.1.0.jar > log.txt &
