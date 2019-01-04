f0a0c40a2a069c9e8dce3beac010897823f4ce38
Andy Shortt <AndrewMShortt@Gmail.com>
2019-01-04 17:09:06 -0500

    changing from direct java process start to init.d

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,8 @@
 # redirect stdout/stderr to a file
 sudo exec &> run.out
 
-cd /usr/bin
-nohup java -jar spring-trade-service-0.1.0.jar &
+sudo link -s /usr/bin/spring-trade-service-0.1.0.jar /etc/init.d/spring-trade-service
+/etc/init.d/myapp start
+
+#cd /usr/bin
+#nohup java -jar spring-trade-service-0.1.0.jar > log.txt &
